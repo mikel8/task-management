edf471390e98e40f0122cf687c1596bf20b3d4f1
sajjad heydari <sajjadheydari1401@gmail.com>
2022-02-26 02:13:51 +0330

    Listing comments

--- src/components/ProjectComments.js
+++ src/components/ProjectComments.js
@@ -1,7 +1,8 @@
 import { useState } from "react"
+import { timestamp } from "../firebase/config"
 import { useFirestore } from "../hooks/useFirestore"
 import { useAuthContext } from "../hooks/useAuthContext"
-import { timestamp } from "../firebase/config"
+import Avatar from "./Avatar"
 
 
 export default function ProjectComments({ project }) {
@@ -31,6 +32,23 @@ export default function ProjectComments({ project }) {
   return (
     <div className="project-comments">
       <h4>Project Comments</h4>
+
+      <ul>
+        {project.comments.length > 0 && project.comments.map(comment => (
+          <li key={comment.id}>
+            <div className="comment-author">
+              <Avatar src={comment.photoURL} />
+              <p>{comment.displayName}</p>
+            </div>
+            <div className="comment-date">
+              <p>date here</p>
+            </div>
+            <div className="comment-content">
+              <p>{comment.content}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
 
       <form className="add-comment" onSubmit={handleSubmit}>
         <label>
