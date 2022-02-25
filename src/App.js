097346c17df1e970638ea535b4833c3c075473e9
sajjad heydari <sajjadheydari1401@gmail.com>
2022-02-25 20:10:31 +0330

    Navbar added, routes fixed

--- src/App.js
+++ src/App.js
@@ -1,41 +1,33 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 // styles
-import './App.css'
+import "./App.css";
 
 // pages & components
-import Dashboard from './pages/dashboard/Dashboard'
-import Create from './pages/create/Create'
-import Login from './pages/login/Login'
-import Signup from './pages/signup/Signup'
-import Project from './pages/project/Project'
+import Dashboard from "./pages/dashboard/Dashboard";
+import Create from "./pages/create/Create";
+import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
+import Project from "./pages/project/Project";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <div className="container">
-          <Switch>
-            <Route exact path="/">
-              <Dashboard />
-            </Route>
-            <Route path="/create">
-              <Create />
-            </Route>
-            <Route path="/projects/:id">
-              <Project />
-            </Route>
-            <Route path="/login">
-              <Login />
-            </Route>
-            <Route path="/signup">
-              <Signup />
-            </Route>
-          </Switch>
+          <Navbar />
+          <Routes>
+            <Route exact path="/" element={<Dashboard />} />
+            <Route path="/create" element={<Create />} />
+            <Route path="/projects/:id" element={<Project />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+          </Routes>
         </div>
       </BrowserRouter>
     </div>
   );
 }
 
-export default App
+export default App;
