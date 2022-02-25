05c96a017c4544cd37048e5d1fcc3b1aa0548e9c
sajjad heydari <sajjadheydari1401@gmail.com>
2022-02-26 02:22:38 +0330

    delete a project done

--- src/components/ProjectSummary.js
+++ src/components/ProjectSummary.js
@@ -1,6 +1,18 @@
-import Avatar from "./Avatar";
+import { useNavigate } from 'react-router-dom'
+import { useAuthContext } from '../hooks/useAuthContext'
+import { useFirestore } from '../hooks/useFirestore'
+import Avatar from './Avatar'
 
 export default function ProjectSummary({ project }) {
+  const { deleteDocument } = useFirestore('projects')
+  const { user } = useAuthContext()
+  const navigate = useNavigate()
+
+  const handleClick = () => {
+    deleteDocument(project.id)
+    navigate('/')
+  }
+
   return (
     <div>
       <div className="project-summary">
@@ -8,16 +20,21 @@ export default function ProjectSummary({ project }) {
         <p className="due-date">
           Project due by {project.dueDate.toDate().toDateString()}
         </p>
-        <p className="details">{project.details}</p>
+        <p className="details">
+          {project.details}
+        </p>
         <h4>Project assigned to:</h4>
         <div className="assigned-users">
-          {project.assignedUsersList.map((user) => (
+          {project.assignedUsersList.map(user => (
             <div key={user.id}>
               <Avatar src={user.photoURL} />
             </div>
           ))}
         </div>
       </div>
+      {user.uid === project.createdBy.id && (
+        <button className="btn" onClick={handleClick}>Mark as Complete</button>
+      )}
     </div>
-  );
+  )
 }
