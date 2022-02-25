1a8f5cbee30be788b1604f5ac2a0764762a2bf0e
sajjad heydari <sajjadheydari1401@gmail.com>
2022-02-25 21:07:47 +0330

    Sigup process finished

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -16,7 +16,7 @@ export default function Sidebar() {
         <nav className="links">
           <ul>
             <li>
-              <NavLink exact to="/">
+              <NavLink exact="true" to="/">
                 <img src={DashboardIcon} alt="dashboard icon" />
                 <span>Dashboard</span>
               </NavLink>
